4c72951b09678433211d4bc8eb939922373b17ae
filip potcoava <filip.potcoava@gmail.com>
2026-08-25 13:30:44 +0300

    added chained operations

--- calcproject/main.cpp
+++ calcproject/main.cpp
@@ -1,54 +1,39 @@
 #include <iostream>
 
 using namespace std;
-
+int n;
 float a,b,x;
 char op;
 bool ok;
 
 int main()
 {
-    cin >> a >> op >> b;
-    switch (op){
+    cin >> n;
+    cin >> x;
+    for (int i=0;i<n;i++) {
+        cin >> op >> b;
+        switch (op){
         case '+':
-            x=a+b;
+            x=x+b;
             break;
         case '-':
-            x=a-b;
+            x=x-b;
             break;
         case '*':
-            x=a*b;
+            x=x*b;
             break;
         case '/':
-            x=a*b;
+            x=x*b;
             break;
         case '%':
-            x=(int)a%(int)b;
+            x=(int)x%(int)b;
             break;
         default:
             cout <<"nu exista";
             ok=true;
+        }
     }
     if (ok==false){
         cout << x;
     }
 }
-//    cin >> a >> op >> b;
-//    //cout << a << " " << op << " " << b;
-//    if (op=='+'){
-//        x=a+b;
-//    }
-//    else if (op=='-'){
-//        x=a-b;
-//    }
-//    else if (op=='*'){
-//        x=a*b;
-//    }
-//    else if (op=='/'){
-//        x=a/b;
-//    }
-//    else if (op=='%'){
-//        x=(int)a%(int)b;
-//    }
-//    cout << x;
-//    return 0;
